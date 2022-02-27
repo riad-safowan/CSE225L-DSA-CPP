d6443eb444050f631fc2cfb46087601dc3e5e878
Riad Safowan <riad.safowan@northsouth.edu>
2022-02-27 23:29:28 +0600

    pass by ...

--- Lab_00_basic_cpp/hello.cpp
+++ Lab_00_basic_cpp/hello.cpp
@@ -27,10 +27,16 @@ int Car::speed(int maxSpeed)
     return maxSpeed;
 }
 
+void testInt(int a, int* b, int& c) {
+    a = a * 10;
+    *b = *b * 10;
+    c = c * 10;
+}
+
 int main()
 {
     cout << "Im Riad Safowan\n";
-    cout << "I love the universe";
+    cout << "I love the universe\n";
 
     // MyClass myObj;
     // myObj.myNum = 15;
@@ -40,7 +46,17 @@ int main()
 
     Car myObj;
     myObj.myMethod();         // Create an object of Car
-    cout << myObj.speed(200); // Call the method with an argument
+    cout << myObj.speed(200)<<endl; // Call the method with an argument
+
+    int a = 5;
+    int bx = 5;
+    int* b = &bx;
+    int cx = 5;
+    int& c = cx;
+
+    cout << a << " " << b << " " << c << endl;
+    testInt(a, b, c);
+    cout << a << " " << b << " " << c << endl;
 
     return 0;
 }
