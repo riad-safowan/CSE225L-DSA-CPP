bbda943a663b1a51d655bf4d9591770b41074c0d
Riad Safowan <riad.safowan@northsouth.edu>
2022-04-18 18:17:45 +0600

    delete leaf and parent with one child

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -32,49 +32,95 @@ node* Insert(node* root, int item) {
 }
 
 void printTreePreOrder(node* root) {
-    if (root == NULL) {
-        return;
-    }
+    if (root == NULL) return;
+
     cout << root->data << " ";
     printTreePreOrder(root->left);
     printTreePreOrder(root->right);
 }
 void printTreeInOrder(node* root) {
-    if (root == NULL) {
-        return;
-    }
+    if (root == NULL) return;
+
     printTreeInOrder(root->left);
     cout << root->data << " ";
     printTreeInOrder(root->right);
 }
 void printTreePostOrder(node* root) {
-    if (root == NULL) {
-        return;
-    }
+    if (root == NULL) return;
+
     printTreePostOrder(root->left);
     printTreePostOrder(root->right);
     cout << root->data << " ";
 }
 
 int findMin(node* root) {
-    if (root->left == NULL)
-    {
-        return root->data;
-    }
-    else {
-        return findMin(root->left);
-    }
-
+    if (root->left == NULL) return root->data;
+    else return findMin(root->left);
 }
 
 int findMax(node* root) {
-    if (root->right == NULL)
-    {
-        return root->data;
+    if (root->right == NULL) return root->data;
+    else return findMax(root->right);
+}
+
+node* findMaxNode(node* root) {
+    if (root->right == NULL) return root;
+    else return findMaxNode(root->right);
+}
+
+bool search(node* root, int key) {
+    if (root == NULL) return false;
+    else if (root->data == key) return true;
+    else if (key < root->data) return search(root->left, key);
+    else return search(root->right, key);
+}
+
+void deleteNode(node* root, int key) {
+    if (root->left == NULL && root->right == NULL) return;
+    //left subtree
+    if (root->left->data == key) {
+        //no child
+        if (root->left->left == NULL && root->left->right == NULL) {
+            delete root->left;
+            root->left = NULL;
+        }
+        //left child
+        else if (root->left->left != NULL && root->left->right == NULL) {
+            node* temp = root->left->left;
+            delete root->left;
+            root->left = temp;
+        }
+        //right child
+        else if (root->left->left == NULL && root->left->right != NULL) {
+            node* temp = root->left->right;
+            delete root->left;
+            root->left = temp;
+        }
+        //todo both subtree exists
     }
-    else {
-        return findMax(root->right);
+    //right subtree
+    else if (root->right->data == key) {
+        //no child
+        if (root->right->left == NULL && root->right->right == NULL) {
+            delete root->right;
+            root->right = NULL;
+        }
+        //left child
+        else if (root->right->left != NULL && root->right->right == NULL) {
+            node* temp = root->right->left;
+            delete root->right;
+            root->right = temp;
+        }
+        //right child
+        else if (root->right->left == NULL && &root->right->right != NULL) {
+            node* temp = root->right->right;
+            delete root->right;
+            root->right = temp;
+        }
+        //todo both subtree exists
     }
+    else if (key < root->data) return deleteNode(root->left, key);
+    else return deleteNode(root->right, key);
 }
 
 int main() {
@@ -85,7 +131,6 @@ int main() {
     root = Insert(root, 10);
     root = Insert(root, 20);
     root = Insert(root, 40);
-    root = Insert(root, 60);
 
     printTreePreOrder(root);
     cout << endl;
@@ -96,4 +141,15 @@ int main() {
     cout << findMin(root);
     cout << endl;
     cout << findMax(root);
+    cout << endl;
+    cout << search(root, 235);
+    cout << endl;
+    deleteNode(root, 50);
+    deleteNode(root, 190);
+    deleteNode(root, 40);
+    cout << endl;
+    printTreeInOrder(root);
+
 }
+
+//height search delete balanced tree 
